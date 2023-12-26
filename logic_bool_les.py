64649ee3c295c6b519aa1d8ef260a26dfe424c25
VergyQA <irinaukhta@mail.ru>
2023-12-26 23:11:03 +0300

    Changes to be committed: 	new file:   RRS_HW_1.py 	modified:   lesson_3_4.py 	new file:   logic_bool_les.py 	new file:   simple_message.py 	new file:   simple_messages.py

---
+++ logic_bool_les.py
@@ -0,0 +1,7 @@
+def my_substr(string, num_cut):
+    i = num_cut - 1
+    test = ''
+    while i <= num_cut:
+        text = text + string[i]
+        i = i + 1
+    return text
---
+++ simple_messages.py
@@ -0,0 +1,4 @@
+message = 'Стойко держись на этом нелегком пути изучения Python!'
+print(message)
+message = 'Long live to the bravest!'
+print(message)
